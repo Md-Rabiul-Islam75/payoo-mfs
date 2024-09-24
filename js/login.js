e1a54735d8b7eb2dc91cbc0bf819b146f7c4a015
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-09-25 01:59:12 +0600

    handle login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -9,13 +9,27 @@
 document.getElementById('button-login').addEventListener('click', function(event){
     // step-2: prevent default behavior (prevent reloading browser)
     event.preventDefault(); // <------ vejal to beginners
-    console.log('login button clicked');
 
-    // step-3: get the phone number
+    // step-3: get the phone number and the pin number
 
     const phoneNumber = document.getElementById('phone-number').value;
+    
+    const pinNumber = document.getElementById('pin-number').value;
 
-    console.log(phoneNumber);
+
+    console.log(phoneNumber, pinNumber);
+  
+   // step-4: validate phone and Pin
+   // this is temporary. You should not do like this
+    if(phoneNumber === '5' && pinNumber === '1234'){
+          console.log('you are logged in');
+          // step - 5: allow user to use the website
+          window.location.href = 'home.html';
+    }
+    else{
+        alert('Wrong phone number or Pin.');
+    }
+
 
 
 })
